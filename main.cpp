b02bf9552ea52f5eeb830cf88ecc4f69461cd209
jamesyang <jamesyang0916@gmail.com>
2023-02-03 14:51:16 +0800

    code refactor

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@
 #include "Student.h"
 #include "LinkedList.h"
 
+void testDs();
+
+void testList();
+
+void testClass();
+
+void testAlg();
+
 using namespace std;
 
 int binarySearch(int arr[], int target, int start, int end) {
@@ -26,43 +34,31 @@ int binarySearch(int arr[], int target, int start, int end) {
 }
 
 int main() {
-    std::vector<int> testVector(1, 9);
-    std::stack<int> testStack;
-    for (int i = 0; i < 10; i++) {
-        testStack.push(i);
-    }
-
-    while (!testStack.empty()) {
-        int element = testStack.top();
-        std::cout << element << std::endl;
-        testStack.pop();
-    }
-
-    std::queue<int> testQueue;
-
-    for (int i = 5; i >= 0; i--) {
-        testQueue.push(i);
-    }
-
-    while (!testQueue.empty()) {
-        std::cout << testQueue.front() << endl;
-        testQueue.pop();
-    }
-
-    Student stu(10, 10, 1);
-    std::cout << stu.getsalary() << std::endl;
-
-    std::cout << "-------------------" << std::endl;
-    int testArr[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int index = binarySearch(testArr, 3, 0, sizeof(testArr) / sizeof(testArr[0]));
-    std::cout << index << endl;
-
-    std::cout << "Hello, World!" <<
+    std::cout << "Hello, cplusplus demo for test!" <<
               std::endl;
 
+    testDs();
+    testClass();
+    testAlg();
+    testList();
+    return 0;
+}
+
+void testAlg() {
+    cout << "-------------------" << endl;
+    int testArr[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int index = binarySearch(testArr, 3, 0, sizeof(testArr) / sizeof(testArr[0]));
+    cout << index << endl;
+}
+
+void testClass() {
+    Student stu(10, 10, 1);
+    cout << stu.getsalary() << endl;
+}
+
+void testList() {
     std::cout << "-------test linked list ----" << std::endl;
     LinkedList list;
-
     for (int i = 10; i < 15; i++) {
         list.add2Tail(i);
     }
@@ -73,20 +69,43 @@ int main() {
 
     for (int i = 0; i < list.length(); i++) {
         ListNode *target = list.findNode(i);
-        std::cout << target->value << std::endl;
+        cout << target->value << endl;
     }
 
     ListNode *target = list.searchKey(8);
-    std::cout << target->value << endl;
+    cout << target->value << endl;
 
 
     list.removeNode(8);
     target = list.searchKey(8);
     if (target == NULL) {
-        std::cout << "remove node success!" << endl;
+        cout << "remove node success!" << endl;
     } else {
-        std::cout << "fail to remove the node." << endl;
+        cout << "fail to remove the node." << endl;
+    }
+}
+
+void testDs() {
+    vector<int> testVector(1, 9);
+    stack<int> testStack;
+    for (int i = 0; i < 10; i++) {
+        testStack.push(i);
     }
 
-    return 0;
+    while (!testStack.empty()) {
+        int element = testStack.top();
+        cout << element << endl;
+        testStack.pop();
+    }
+
+    queue<int> testQueue;
+
+    for (int i = 5; i >= 0; i--) {
+        testQueue.push(i);
+    }
+
+    while (!testQueue.empty()) {
+        cout << testQueue.front() << endl;
+        testQueue.pop();
+    }
 }
